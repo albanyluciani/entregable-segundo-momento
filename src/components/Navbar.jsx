8be002c745093568f70edc4edac477a1dd604b10
Albany Luciani <lucianialbany@gmail.com>
2026-04-21 19:33:46 -0500

    organización de encarpetado

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+const Navbar = () => {
+  return (
+    <nav> 
+      <a href="">Zenith Bonsai</a>
+       <div>
+       <a href="">Filosofía</a>
+      <a href="">Galería</a>
+      <a href="">Reservas</a>
+      </div>
+    </nav>  
+  )
+}
+
+export default Navbar
